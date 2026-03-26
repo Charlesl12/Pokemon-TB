8e65818c204ac5bc38c662a0331231655309f94b
Charlesl12 <charles-1614@hotmail.com>
2026-03-27 01:00:10 +0800

    Fix incorrect file extension

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { TeamProvider} from "./context/TeamContext.js";
+import { TeamProvider} from "./context/TeamContext.jsx";
 import './index.css'
 import App from './App.jsx'
 
